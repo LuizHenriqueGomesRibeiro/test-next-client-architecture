8f9d624748c9beb16cd3e7ab0ad1ea97a0a80bfc
Luiz Gomes <louisnuuk@hotmail.com>
2025-03-29 16:00:09 -0300

    feat

--- src/services/api/server/index.ts
+++ src/services/api/server/index.ts
@@ -7,9 +7,13 @@ function createApiClass<T extends ApiConfig>(list: T) {
     return class Api {
         constructor() {
             Object.keys(list).forEach((key) => {
-                (this as any)[key] = async (params?: any) => {
-                    return this.request(list[key].method, list[key].url, list[key].authenticated);
-                };
+                if (list[key]) {
+                    (this as any)[key] = async (params?: any) => {
+                        return this.request(list[key].method, list[key].url, list[key].authenticated);
+                    };
+                } else {
+                    console.error(`A chave ${key} está indefinida no objeto api.`);
+                }
             });
         }
     
