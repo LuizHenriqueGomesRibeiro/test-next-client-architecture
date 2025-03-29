3c26db1893bc75e6682b9e4ea4f8328e4b0c96f9
Luiz Gomes <louisnuuk@hotmail.com>
2025-03-29 16:11:43 -0300

    feat

--- src/api/index.ts
+++ src/api/index.ts
@@ -3,9 +3,14 @@ import { ApiEndpoint } from "../services/endpoints/types";
 export const BASE_URL = "https://dog.ceo/api";
 
 export const api = {
-    requestc: {
+    breeds_image_random: {
         url: "breeds/image/random",
         authenticated: false,
         method: 'get'
-    }
+    },
+    breed_hound_images: {
+        url: "breed/hound/images",
+        authenticated: false,
+        method: 'get'
+    },
 } as const satisfies Record<string, ApiEndpoint>;
